32f5b76aaefb692c59636dd3f94a62386d07e4ac
Marcus Rugger <marcusrugger@yahoo.com>
2019-08-16 21:36:48 +0000

    Add validation

--- csharp/program.cs
+++ csharp/program.cs
@@ -21,7 +21,13 @@ namespace StackMachine
             foreach (var test in testlist)
             {
                 foreach (var op in test) op(stack);
-                Console.WriteLine($"Result: {stack.Pop()}");
+                var shouldbe = stack.Pop();
+                var actual = stack.Pop();
+                var passed = actual.Equals(shouldbe);
+                if (passed)
+                    Console.WriteLine($"Test PASSED: result {actual}");
+                else
+                    Console.WriteLine($"Test FAILED: should be {shouldbe}, actual {actual}");
             }
         }
 
@@ -38,7 +44,9 @@ namespace StackMachine
             Operators.Multiply,
 
             Operators.Push(4.27),
-            Operators.Multiply
+            Operators.Multiply,
+
+            Operators.Push(186282.3976*3600*24*365.25*4.27)
         };
 
         static List<Operator> TestSquareRoot => new List<Operator>
@@ -46,7 +54,9 @@ namespace StackMachine
             Operators.Push(2),
             Operators.Push(8),
             Operators.Multiply,
-            Operators.SquareRoot
+            Operators.SquareRoot,
+
+            Operators.Push(4)
         };
 
         static List<Operator> TestSquaresAndSquareRoot => new List<Operator>
@@ -58,21 +68,27 @@ namespace StackMachine
             Operators.Square,
 
             Operators.Add,
-            Operators.SquareRoot
+            Operators.SquareRoot,
+
+            Operators.Push(5)
         };
 
         static List<Operator> TestDivide => new List<Operator>
         {
             Operators.Push(1.0),
             Operators.Push(2.0),
-            Operators.Divide
+            Operators.Divide,
+
+            Operators.Push(0.5)
         };
 
         static List<Operator> TestRemainder => new List<Operator>
         {
             Operators.Push(3.5),
             Operators.Push(2.1),
-            Operators.Remainder
+            Operators.Remainder,
+
+            Operators.Push(1.4)
         };
     }
 }
